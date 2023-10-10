f20c6263e11c9a7cdc1489a2f8b7001b0da1adc1
PedroAugustoCruz <oliveiracruzpedroaugusto@gmail.com>
2023-10-10 20:56:09 -0300

    testando as funcionalidades

--- teste.py
+++ teste.py
@@ -3,6 +3,10 @@ import handle
 
 tupla = ("Pedro", "BCMT", 19, 9.5)
 
-tupla_nova = handle.incrementByHalf(tupla, "primeiro período")
+tupla_metade = handle.incrementByHalf(tupla, "primeiro período")
+tupla_indice = handle.incrementByPosition(tupla, "primeiro período", 3)
+tupla_comeco = handle.incrementInStart(tupla, "primeiro período")
 
-print(tupla_nova)
+print(tupla_metade)
+print(tupla_indice)
+print(tupla_comeco)
