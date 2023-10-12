70ff01fe0c6b70020d752f1664eef00007da446a
PedroAugustoCruz <oliveiracruzpedroaugusto@gmail.com>
2023-10-12 16:51:35 -0300

    Reescrevendo as funções e adicionando nova funcionalidade na func half

--- handle.py
+++ handle.py
@@ -1,38 +1,49 @@
 def testLenth(tupla):
   """Função que testa se a tupla é par"""
-  possibilidade = len(tupla)/2
-
-  testa = str(possibilidade)
-
+  testa = str(len(tupla)/2)
   apos_ponto = testa[testa.index("."):len(tupla)]      
   if apos_ponto[0:3] != ".0":
-    raise Exception("Tupla não é par")
-  return 
+    return False
+  else:
+    return True
 
 
+def final(tupla):
+  return len(tupla) + 1
 
+# def testType(novo_elemento):
+#   if type(novo_elemento) == int or str or float :
+#     return True
+#   else:
+#     return False
 
 def incrementByPosition(tupla, novo_elemento, posicao):
   """Função que adiciona um novo elemento em uma tupla em um indice desejado"""
-  tamanho = len(tupla)
-  nova_tupla = tupla[0:posicao] + (novo_elemento,) + tupla[posicao: tamanho]
+  nova_tupla = tupla[0:posicao] + (novo_elemento,) + tupla[posicao:final(tupla)]
   return nova_tupla
 
 
 
-def incrementByHalf(tupla, novo_elemento):
+def incrementByHalf(tupla, novo_elemento, posicao=0):
   """Função que adiciona um novo elemento na metade de uma tupla"""
-  testLenth(tupla)
-  tamanho = len(tupla)
-  metade = len(tupla)//2
-  nova_tupla = tupla[0:metade] + (novo_elemento,) + tupla[metade:tamanho]
-
+  if posicao == 0 and testLenth(tupla)==True:
+    metade = len(tupla)//2
+    nova_tupla = tupla[0:metade] + (novo_elemento,) + tupla[metade:final(tupla)]
+  elif posicao == -1:
+    testLenth(tupla)
+    metade = len(tupla)//2
+    nova_tupla = tupla[0:metade-1] + (novo_elemento,) + tupla[metade-1:final(tupla)]
+  elif posicao == 1:
+    testLenth(tupla)
+    metade = len(tupla)//2
+    nova_tupla = tupla[0:metade+1] + (novo_elemento,) + tupla[metade+1:final(tupla)]
+  else:
+    raise Exception("Se a tupla tiver um numero ímpar de elementos, indique se o novo elemento adicionado e mais para o começo ou final da tupla faça com 1 ou -1")
   return nova_tupla
 
 
 def incrementInStart(tupla, novo_elemento):
   """Função que adiciona um elemento no começo da tupla"""  
-  tamanho = len(tupla)
-  nova_tupla = (novo_elemento,) + tupla[0:tamanho]
+  nova_tupla = (novo_elemento,) + tupla[0:final(tupla)]
   
   return nova_tupla
--- teste.py
+++ teste.py
@@ -1,9 +1,9 @@
 import handle
 
 
-tupla = ("Pedro", "BCMT", 19, 9.5)
+tupla = ("Pedro", "BCMT", 19, 9.5, 8)
 
-tupla_metade = handle.incrementByHalf(tupla, "primeiro período")
+tupla_metade = handle.incrementByHalf(tupla, "primeiro período", -1)
 tupla_indice = handle.incrementByPosition(tupla, "primeiro período", 3)
 tupla_comeco = handle.incrementInStart(tupla, "primeiro período")
 
